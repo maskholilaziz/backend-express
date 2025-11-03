a0fd91ed276ca6ce3e208156e2df4f4fa901bf79
Muhammad Kholil Aziz Ashari <muhammadkholilazizashari@gmail.com>
2025-11-03 14:40:17 +0700

    Add CORS & Body Parser

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // import exporess
 const express = require("express");
 
+// import CROS
+const cors = require("cors");
+
+// import bodyParse
+const bodyParser = require("body-parser");
+
 // init app
 const app = express();
+
+// use CROS
+app.use(cors());
+
+// use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // define port
 const port = 3000;
