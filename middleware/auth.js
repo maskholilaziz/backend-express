edc4c5e15db5204050a037dad9220c80eb1cdd91
Muhammad Kholil Aziz Ashari <muhammadkholilazizashari@gmail.com>
2025-11-03 15:11:00 +0700

    Add JWT

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+// Import express
+const express = require("express");
+
+// Import jsonwebtoken
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    // Get token
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthenticated."
+        });
+    }
+
+    // Verify token
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({ message: "Invalid token." });
+
+        req.userId = decode.id;
+        next();
+    })
+}
+
+module.exports = verifyToken;
