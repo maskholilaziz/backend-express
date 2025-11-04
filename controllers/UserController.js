86e2190eca58483343e138bd5b7be66dbc73a820
Muhammad Kholil Aziz Ashari <muhammadkholilazizashari@gmail.com>
2025-11-04 11:12:04 +0700

    Add Create Data User

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,12 @@ const express = require("express");
 // import prisma client
 const prisma = require("../prisma/client");
 
+// Import validationResult
+const { validationResult } = require("express-validator");
+
+// import bcryptjs
+const bcryptjs = require("bcryptjs");
+
 // function findUsers
 const findUsers = async (req, res) => {
     try {
@@ -38,4 +44,46 @@ const findUsers = async (req, res) => {
     }
 }
 
-module.exports = { findUsers };
+// function createUser
+const createUser = async (req, res) => {
+    // Pemeriksa hasil validasi
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // Jika ada errpr, kembalikan error ke pengguna
+        return res.status(422).json({
+            success: false,
+            message: "Validation Error",
+            errors: errors.array()
+        });
+    }
+
+    // hash password
+    const hashedPassword = await bcryptjs.hash(req.body.password, 10);
+
+    try {
+        // insert data
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            }
+        });
+
+        // kembalikan respon
+        res.status(201).json({
+            success: true,
+            message: "Register successfully",
+            data: user
+        });
+    } catch (error) {
+        // kembalikan error
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: error.message
+        });
+    }
+}
+
+module.exports = { findUsers, createUser };
